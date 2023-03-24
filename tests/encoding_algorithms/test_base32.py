8f61fa8b4f5d6f9a13cc966dffe357733cf4e9cb
Rusih100 <rusih100@gmail.com>
2023-03-25 00:52:59 +1000

    Дополнительные тесты кодирования base32

--- tests/encoding_algorithms/test_base32.py
+++ tests/encoding_algorithms/test_base32.py
@@ -1,3 +1,6 @@
+import base64
+from random import randbytes, randint
+
 import pytest
 
 from encoding_algorithms.base32 import base32_decode, base32_encode
@@ -18,3 +21,9 @@ class TestBase32:
     )
     def test_encode(self, input_bytes: bytes, expected_bytes: bytes):
         assert base32_encode(input_bytes) == expected_bytes
+
+    @pytest.mark.parametrize(  # Тесты на случайных байтах
+        "input_bytes", [(randbytes(randint(0, 20))) for i in range(10)]
+    )
+    def test_encode_random(self, input_bytes: bytes):
+        assert base32_encode(input_bytes) == base64.b32encode(input_bytes)
