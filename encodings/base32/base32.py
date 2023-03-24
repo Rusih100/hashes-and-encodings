21d80e164b59c33f040604c9e3d7561634862218
Rusih100 <rusih100@gmail.com>
2023-03-24 15:03:51 +1000

    Заготовки для base32: таблица кодирования, функция для реверса таблицы кодирования

---
+++ encodings/base32/base32.py
@@ -0,0 +1,6 @@
+def base32_encode(data_bytes: bytes) -> bytes:
+    pass
+
+
+def base32_decode(base64_bytes: bytes) -> bytes:
+    pass
---
+++ tests/utils/test_reverse_dict.py
@@ -0,0 +1,18 @@
+import pytest
+
+from core.utils.reverse_dict import reverse_dict
+
+
+@pytest.mark.parametrize(
+    "input_dict, expected_dict",
+    [
+        (
+            {0b00000: b"A", 0b00001: b"B", 0b00010: b"C", 0b00011: b"D"},
+            {b"A": 0b00000, b"B": 0b00001, b"C": 0b00010, b"D": 0b00011},
+        ),
+        ({"A": 1}, {1: "A"}),
+        ({}, {}),
+    ],
+)
+def test_reverse_dict(input_dict, expected_dict):
+    assert reverse_dict(input_dict) == expected_dict
