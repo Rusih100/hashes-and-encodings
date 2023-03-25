68754bfd0371e34525d47cae24d14c9c2b1c4eb8
Rusih100 <rusih100@gmail.com>
2023-03-25 12:45:08 +1000

    Тесты декодирования для base32

--- tests/encoding_algorithms/test_base32.py
+++ tests/encoding_algorithms/test_base32.py
@@ -27,3 +27,24 @@ class TestBase32:
     )
     def test_encode_random(self, input_bytes: bytes):
         assert base32_encode(input_bytes) == base64.b32encode(input_bytes)
+
+    @pytest.mark.parametrize(
+        "input_bytes, expected_bytes",
+        [
+            (b"", b""),
+            (b"MY======", b"f"),
+            (b"MZXQ====", b"fo"),
+            (b"MZXW6===", b"foo"),
+            (b"MZXW6YQ=", b"foob"),
+            (b"MZXW6YTB", b"fooba"),
+            (b"MZXW6YTBOI======", b"foobar"),
+        ],
+    )
+    def test_decode(self, input_bytes: bytes, expected_bytes: bytes):
+        assert base32_decode(input_bytes) == expected_bytes
+
+    @pytest.mark.parametrize(  # Тесты на случайных байтах
+        "input_bytes", [(randbytes(randint(0, 20))) for j in range(10)]
+    )
+    def test_reversibility_random(self, input_bytes: bytes):
+        assert base32_decode(base32_encode(input_bytes)) == input_bytes
