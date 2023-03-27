f42e803432db667d537d84f7531ada3f657f60c8
Rusih100 <rusih100@gmail.com>
2023-03-27 19:49:14 +1000

    Рабочий base64 с тестами к нему

---
+++ encoding_algorithms/base64/__init__.py
@@ -0,0 +1,1 @@
+from encoding_algorithms.base64.base64 import base64_encode, base64_decode
--- encoding_algorithms/base64/base64.py
+++ encoding_algorithms/base64/base64.py
@@ -1,45 +1,41 @@
-from encoding_algorithms.base32.tables import DECODING_TABLE, ENCODING_TABLE
+from encoding_algorithms.base64.tables import DECODING_TABLE, ENCODING_TABLE
 
 
-def base32_encode(data_bytes: bytes) -> bytes:
+def base64_encode(data_bytes: bytes) -> bytes:
     encoded_string: bytes = b""
     length_data = len(data_bytes)
     count_signs = 0
 
-    for block_index in range(0, length_data, 5):
-        bytes_count = min(5, length_data - block_index)
+    for block_index in range(0, length_data, 3):
+        bytes_count = min(3, length_data - block_index)
 
         block = int.from_bytes(
-            data_bytes[block_index : block_index + 5], byteorder="big"
+            data_bytes[block_index: block_index + 3], byteorder="big"
         )
 
         bit_count = bytes_count * 8
         while bit_count > 0:
-            if bit_count >= 5:
-                int_key = (block >> (bit_count - 5)) & 0b00011111
+            if bit_count >= 6:
+                int_key = (block >> (bit_count - 6)) & 0b00111111
             else:
-                int_key = (block & (0b00011111 >> (5 - bit_count))) << (
-                    5 - bit_count
+                int_key = (block & (0b00111111 >> (6 - bit_count))) << (
+                        6 - bit_count
                 )
                 match bit_count:
-                    case 3:
-                        count_signs = 6
-                    case 1:
-                        count_signs = 4
                     case 4:
-                        count_signs = 3
-                    case 2:
                         count_signs = 1
+                    case 2:
+                        count_signs = 2
 
             encoded_string += ENCODING_TABLE[int_key]
-            bit_count -= 5
+            bit_count -= 6
 
     encoded_string += b"=" * count_signs
 
     return encoded_string
 
 
-def base32_decode(data_bytes: bytes) -> bytes:
+def base64_decode(data_bytes: bytes) -> bytes:
     decoded_string: bytes = b""
     count_signs = 0
 
@@ -50,30 +46,24 @@ def base32_decode(data_bytes: bytes) -> bytes:
     length_data = len(data_bytes)
 
     for block_index in range(0, length_data, 8):
-        bytes_block: bytes = data_bytes[block_index : block_index + 8]
+        bytes_block: bytes = data_bytes[block_index: block_index + 8]
         length_bytes_block = len(bytes_block)
 
         block = 1
         bit_count = 0
         for i in range(length_bytes_block):
-            byte_key: bytes = bytes_block[i : i + 1]
-            block = (block << 5) | (DECODING_TABLE[byte_key])
-            bit_count += 5
+            byte_key: bytes = bytes_block[i: i + 1]
+            block = (block << 6) | (DECODING_TABLE[byte_key])
+            bit_count += 6
 
         if block_index + 8 > length_data:
             match count_signs:
-                case 6:
+                case 1:
                     block = block >> 2
                     bit_count -= 2
-                case 4:
+                case 2:
                     block = block >> 4
                     bit_count -= 4
-                case 3:
-                    block = block >> 1
-                    bit_count -= 1
-                case 1:
-                    block = block >> 3
-                    bit_count -= 3
 
         temp_byte_string: bytes = b""
 
