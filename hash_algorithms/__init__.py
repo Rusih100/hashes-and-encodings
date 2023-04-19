e507c4111db72566bbf0a6f2b5f33d6d0f5b1e9b
Rusih100 <rusih100@gmail.com>
2023-04-19 19:13:00 +1000

    Стрибог

--- hash_algorithms/__init__.py
+++ hash_algorithms/__init__.py
@@ -1,3 +1,3 @@
+from hash_algorithms.hmac import hmac
 from hash_algorithms.sha import sha256, sha512
 from hash_algorithms.stribog import stribog256, stribog512
-from hash_algorithms.hmac import hmac
--- hash_algorithms/stribog/__init__.py
+++ hash_algorithms/stribog/__init__.py
@@ -1,2 +1,2 @@
-from hash_algorithms.stribog.stribog512 import stribog512
 from hash_algorithms.stribog.stribog256 import stribog256
+from hash_algorithms.stribog.stribog512 import stribog512
